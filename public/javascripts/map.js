f0f6184b0faa88ea5a8e549d00493404543e5a4b
leofishman <leofishman@gmail.com>
2021-03-04 18:18:30 -0300

    Api crud (falta update)

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -5,3 +5,17 @@ L.tileLayer(
 {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
+
+
+$.ajax({
+    dataType: "json",
+    url: "api/bicicletas",
+    success: function(result){
+        console.log(result)
+        result.bicicletas.forEach(function(bici){
+            L.marker(bici.ubicacion,{title: bici.id}).addTo(map);
+        });
+    }
+})
+
+
