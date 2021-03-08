6433951a2a4a6f8f1aab3178f47c557f1d37956f
leofishman <leofishman@gmail.com>
2021-03-08 09:13:15 -0300

    reservar method en usuario

--- models/usuarios.js
+++ models/usuarios.js
@@ -9,4 +9,16 @@ const usuarioSchema = new Schema({
   }
 });
 
+
+usuarioSchema.methods.reservar = function (biciId, desde, hasta, cb) {
+    var reserva = new Reserva({
+      usuario: this._id,
+      bicicleta: biciId,
+      desde,
+      hasta
+    })
+    console.log(reserva)
+    reserva.save(cb);
+  }  
+
 module.exports = mongoose.model('Usuario', usuarioSchema)
