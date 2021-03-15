0598953417556fb4f9727dcf50626e91ac865b41
leofishman <leofishman@gmail.com>
2021-03-15 10:14:29 -0300

    model usuario pass, email

--- models/usuarios.js
+++ models/usuarios.js
@@ -3,10 +3,37 @@ const Reserva = require('./reserva')
 
 const Schema = mongoose.Schema;
 
+const validateEmail = function(email){
+  const re = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+  return re.test(email);
+};
+
+
 const usuarioSchema = new Schema({
   nombre: {
     type: String,
-  }
+    trim: true,
+    required: [true, 'El nombre es obligatorio']
+  },
+  email: {
+      type: String,
+      trim: true,
+      required: [true, 'El email es obligatorio'],
+      lowercase: true,
+      unique: true,
+      validate:[validateEmail, 'Por favor, ingrese un email válido'],
+      match: [/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/]
+  },
+  password: {
+      type: String,
+      required: [true, 'El password es obligatorio']
+  },
+  passwordResetToken: String,
+  passwordResetTokenExpires: Date,
+  verificado: {
+    type: Boolean,
+    default: false
+  },
 });
 
 
