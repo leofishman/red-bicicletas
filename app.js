b72cd4eede603ba48c10e1b617c85ee862db0184
leofishman <leofishman@gmail.com>
2021-03-21 09:27:57 -0300

    passport

--- app.js
+++ app.js
@@ -3,13 +3,25 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const passport = require('./config/passport');
+const session = require('express-session');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var bicicletasRouter = require('./routes/bicicletas');
 var bicicletasAPIRouter = require('./routes/api/bicicletas');
+var usuariosApisRouter = require('./routes/api/usuarios');
+
+const store = new session.MemoryStore;
 
 var app = express();
+app.use(session.({
+  cookie: { maxAge: 240 * 60 * 60 * 1000 },
+  store: store,
+  saveUninitialized: true,
+  resave: 'true',
+  secret: 'sdfghjsaj89078saf09'
+}));
 
 var mongoose = require('mongoose');
 
@@ -37,6 +49,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
